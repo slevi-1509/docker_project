3e503cb6e1e6d6c8c564efebc879355408a05ba0
slevi-1509 <sagivlevi1509@gmail.com>
2023-08-22 15:38:33 +0300

    9 seconds

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 docker build -t docker-project .
 docker run -d --rm --name docker-project docker-project
 sleep 15
-if [ "$( docker container inspect -f '{{.State.Running}}' docker-project )" == "true" ]
-then
-        echo "Docker is still up, you fucked up !!!"
+if [ "$( docker container inspect -f '{{.State.Running}}' docker-project )" == "true" ]; then
+	echo "Docker is still up, you fucked up !!!"
+    exit 1
 else
-        echo "It's all good, man"
+	echo "It's all good, man !!!"
 fi
